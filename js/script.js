f039fde7e8b293b8ef74a704745437189743f521
gabryelbo7 <gabryelborgeslima@gmail.com>
2026-03-18 18:05:41 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Arquivo JavaScript
+// Aqui você pode adicionar interatividade futura, como um player de música personalizado
+// ou animações ao rolar a página.
+console.log("Site carregado com sucesso!");
